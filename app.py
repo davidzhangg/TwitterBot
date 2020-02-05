0a3813042be26fef4be95d109f6c767275389754
David Zhang <59639140+davidzhangg@users.noreply.github.com>
2020-02-04 23:34:10 -0800

    added comments

--- app.py
+++ app.py
@@ -11,27 +11,41 @@ class TwitterBot:
 
     def login(self):
         bot = self.bot
+        #opens up twitter page
         bot.get('https://twitter.com/')
+        #pause for 3 seconds to let everything load up
         time.sleep(3)
+        #grabs the class name of the email box
         email = bot.find_element_by_name('session[username_or_email]')
+        #grabs the class name of the password box
         password = bot.find_element_by_name('session[password]')
+        #clear any email already present in the email box
         email.clear()
+        #clear any password already present in the password box
         password.clear()
+        #enters in username/email
         email.send_keys(self.username)
+        #enters in password
         password.send_keys(self.password)
+        #press login
         password.send_keys(Keys.RETURN)
+        #wait for 3 seconds for everything to load
         time.sleep(3)
 
     def like_tweet(self,search):
         bot = self.bot
+        #search for certain topics on twitter to go to in the search box
         bot.get('https://twitter.com/search?q=' + search + '&src=typed_query')
         time.sleep(3)
+        #scrolls through the twitter page 
         for i in range(1, 3):
             bot.execute_script('window.scrollTo(0, document.body.scrollHeight)')
             time.sleep(2)
+            #grabs the tweets
             tweets = bot.find_elements_by_class_name('css-1dbjc4n')
+            #saves the url of each tweet
             links = [elem.get_attribute('data-permalink-path') for elem in tweets]
-    
+    		#for each tweet, press the like button 
             for link in links:
                 bot.get('https://twitter.com' + link)
                 try:
